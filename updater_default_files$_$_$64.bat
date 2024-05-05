68c679eb34fbc7a6781d865da92c8834a2228eda
Joerg von Roos <40567703+jvr-ks@users.noreply.github.com>
2024-05-05 17:13:34 +0200

    0.050

---
+++ updater_default_files$_$_$64.bat
@@ -0,0 +1,18 @@
+@rem updater_default_files$_$_$64.bat
+
+xcopy /y /f "dottylatest.jar" "dottylatest.jar.default"
+
+xcopy /y /f "dottylatest_Java.bat" "dottylatest_Java.bat.default"
+
+xcopy /y /f "dottylatest_Java.lnk" "dottylatest_Java.lnk.default"
+
+xcopy /y /f "dottylatest_Scala.bat" "dottylatest_Scala.bat.default"
+
+xcopy /y /f "dottylatest_Scala.lnk" "dottylatest_Scala.lnk.default"
+
+xcopy /y /f "dottylatest.exe" "dottylatest.exe.default"
+
+xcopy /y /f "loader.ini" "loader.ini.default"
+
+xcopy /y /f "MIT-License.txt" "MIT-License.txt.default"
+
